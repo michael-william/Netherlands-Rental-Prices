514c7d05fcd754b7373c2f40e760047daa4af42b
michael-william <56475992+michael-william@users.noreply.github.com>
2020-07-04 07:31:54 +0200

    optimized app

--- Dutch_rental_predictions.py
+++ Dutch_rental_predictions.py
@@ -20,68 +20,95 @@ model to predict the price of a property basedon user inputs. Find the lat and l
 address from https://getlatlong.net/
 """)
 
-st.sidebar.header('Rental Parameters')
+@st.cache
+def load_data(filename=None):
+    data_source = 'https://github.com/michael-william/Netherlands-Rental-Prices/raw/master/final_ml_data.csv'
+    df=pd.read_csv(data_source)
+    y = df.rent
+    X = df[['areaSqm','longitude','latitude','propertyType']]
+    return df, X, y
+
+df, X, y = load_data()
+
+@st.cache(allow_output_mutation=True)
+def run_ml_model():    
+    categorical_cols = ['propertyType']
+
+    numerical_cols = ['areaSqm','longitude','latitude']
+
+    # Preprocessing for numerical data
+    numerical_transformer = SimpleImputer(strategy='median')
+
+    # Preprocessing for categorical data
+    categorical_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='most_frequent')),
+        ('label', OneHotEncoder(handle_unknown='ignore'))])
+
+    # Grouping numeric and categorical preprocessing 
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', numerical_transformer, numerical_cols),
+            ('cat', categorical_transformer, categorical_cols)])
+
+    # Define model
+    model = XGBRegressor(n_estimators=1100, learning_rate=0.05)
+
+    # Pipeline for processing the data and defining the model
+    clf = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('model', model)])
+
+    clf.fit(X,y)
+    mae = '€'+str(111)
+    return clf, model, mae
+
+clf, model, mae = run_ml_model()
 
 def user_input_features():
-    square_meters = st.sidebar.slider('Area in square meters', 6, 675, 56)
-    latitude = st.sidebar.slider('Latitude', 50.770041, 53.333967, 51.2)
-    longitude = st.sidebar.slider('Longitude', 3.554188, 7.036756, 5.2)
-    p_type = st.sidebar.selectbox('Apartment',['Room', 'Studio', 'Apartment', 'Anti-squat', 'Student residence'])
-    data = {'areaSqm': square_meters,
-            'longitude': longitude,
-            'latitude': latitude,
-            'propertyType': p_type}
-    features = pd.DataFrame(data, columns = ['areaSqm','longitude','latitude', 'propertyType'], index=[0])
-    return features
+        square_meters = st.sidebar.slider('Area in square meters', 6, 675, 56)
+        latitude = st.sidebar.slider('Latitude', 50.770041, 53.333967, 51.2)
+        longitude = st.sidebar.slider('Longitude', 3.554188, 7.036756, 5.2)
+        p_type = st.sidebar.selectbox('Apartment',['Room', 'Studio', 'Apartment', 'Anti-squat', 'Student residence'])
+        data = {'areaSqm': square_meters,
+                'longitude': longitude,
+                'latitude': latitude,
+                'propertyType': p_type}
+        features = pd.DataFrame(data, columns = ['areaSqm','longitude','latitude', 'propertyType'], index=[0])
+        return features
 
-user_df = user_input_features()
-
-st.subheader('Rental parameters')
-
-st.write(user_df)
-
-data_source = 'https://github.com/michael-william/Netherlands-Rental-Prices/raw/master/final_ml_data.csv'
-df=pd.read_csv(data_source)
-
-# Remove rows with missing target, separating target from predictors
-df.dropna(axis=0, subset=['rent'], inplace=True)
-y = df.rent
-X = df[['areaSqm','longitude','latitude','propertyType']]
-
-categorical_cols = ['propertyType']
-
-numerical_cols = ['areaSqm','longitude','latitude']
-
-# Preprocessing for numerical data
-numerical_transformer = SimpleImputer(strategy='median')
-
-# Preprocessing for categorical data
-categorical_transformer = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='most_frequent')),
-    ('label', OneHotEncoder(handle_unknown='ignore'))])
-
-# Grouping numeric and categorical preprocessing 
-preprocessor = ColumnTransformer(
-    transformers=[
-        ('num', numerical_transformer, numerical_cols),
-        ('cat', categorical_transformer, categorical_cols)])
-
-# Define model
-model = XGBRegressor(n_estimators=1100, learning_rate=0.05)
-
-# Pipeline for processing the data and defining the model
-clf = Pipeline(steps=[
-('preprocessor', preprocessor),
-('model', model)])
-
-clf.fit(X,y)
-mae = '€'+str(111)
+def main():
     
-prediction = '€'+str(round(clf.predict(user_df)[0],0))
 
-st.subheader('Rental prediction')
-st.write(prediction)
+    st.sidebar.header('Rental Parameters')
 
-st.subheader('Error range')
-st.write(mae)
+
+    user_df = user_input_features()
+
+    st.subheader('Rental parameters')
+
+    st.write(user_df)
+
+    load_data()
+
+    
+
+    def predict():
+        prediction = '€'+str(int(round(clf.predict(user_df)[0],0)))
+
+        st.subheader('Monthly rental prediction')
+        st.write(prediction)
+
+    if st.sidebar.button('Predict'):
+        predict()
+
+        st.subheader('+/- range')
+        st.write(mae)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
 
